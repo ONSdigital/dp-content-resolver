327f3f83267b296f26955281133db9fb0c76f4b8
Carl Hembrough <carlhuk@gmail.com>
2016-10-20 16:44:41 +0100

    added resolveHandler and resolver

---
+++ handlers/resolveHandler.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"github.com/ONSdigital/dp-content-resolver/resolvers"
+	"github.com/ONSdigital/go-ns/log"
+	"net/http"
+)
+
+// Handler interface
+type Handler interface {
+	Handle(w http.ResponseWriter, req *http.Request)
+}
+
+// ResolveHandler deals with the http request and forwards the extracted url onto the resolver.
+type ResolveHandler struct {
+	Resolver resolvers.Resolver
+}
+
+// Handle will resolve the page defined by the path.
+func (handler *ResolveHandler) Handle(w http.ResponseWriter, req *http.Request) {
+
+	log.DebugR(req, "Resolver handler", nil)
+
+	data, err := handler.Resolver.Resolve(req.URL.Query().Get("uri"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(data)
+}
