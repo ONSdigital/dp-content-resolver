56a69c6989d40b64659b57b9502a4cf8f3fc270d
Dave Llewellyn <439837@gmail>
2016-10-20 13:48:17 +0100

    Changes

--- handlers/resolver/handler.go
+++ handlers/resolver/handler.go
@@ -8,13 +8,13 @@ import (
 
 // Handler will resolve the page defined by the path.
 func Handler(w http.ResponseWriter, req *http.Request) {
-
 	log.DebugR(req, "Resolver handler", nil)
 
-	data, err := resolver.Resolve(req.URL.Path)
+	data, err := resolver.Resolve(req.URL.Query().Get("uri"))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
 	w.Write(data)
 }
