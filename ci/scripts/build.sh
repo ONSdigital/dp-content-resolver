a62faead5f5040354cc68a2e1702eb074e068124
Lloyd Griffiths <lloyd.griffiths@ons.gov.uk>
2017-06-09 17:16:45 +0100

    copy Dockerfile to build directory

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -4,5 +4,5 @@ export BINPATH=$(pwd)/build
 export GOPATH=$(pwd)/go
 
 pushd $GOPATH/src/github.com/ONSdigital/dp-content-resolver
-  go build -o $BINPATH/dp-content-resolver
+  go build -o $BINPATH/dp-content-resolver && cp Dockerfile.concourse $BINPATH/
 popd
